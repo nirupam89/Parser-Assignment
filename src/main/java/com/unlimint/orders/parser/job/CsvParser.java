f20a54ed9360ea4b33254caf13fab7f6e01c8ea8
Nirupam Das <nirupam@Nirupams-MacBook-Pro.local>
2022-09-21 13:58:24 +0530

    remove commented code

--- src/main/java/com/unlimint/orders/parser/job/CsvParser.java
+++ src/main/java/com/unlimint/orders/parser/job/CsvParser.java
@@ -4,17 +4,13 @@ import java.io.File;
 import java.io.FileReader;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.springframework.stereotype.Component;
 
 import com.opencsv.CSVReader;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
 import com.unlimint.orders.parser.enums.Result;
 import com.unlimint.orders.parser.model.Order;
 
@@ -27,36 +23,9 @@ public class CsvParser implements DataParser {
 	public List<Order> parse(String fileName) {
 		List<Order> orders = new ArrayList<>();
 		try {
-//			File file = new File(getClass().getResource(fileName).getFile());
 			FileReader filereader = new FileReader(fileName);
 			CSVReader csvReader = new CSVReader(filereader);
 
-			/*Map<String, String> mapping = new HashMap<String, String>();
-			mapping.put("Order ID", "orderId");
-			mapping.put("amount", "amount");
-			mapping.put(" amount", "amount");
-			mapping.put("currency", "currency");
-			mapping.put(" currency", "currency");
-			mapping.put("comment", "comment");
-			mapping.put(" comment", "comment");
-
-			HeaderColumnNameTranslateMappingStrategy<Order> strategy = new HeaderColumnNameTranslateMappingStrategy<Order>();
-			strategy.setType(Order.class);
-			strategy.setColumnMapping(mapping);
-
-			
-			CsvToBean<Order> csvToBean = new CsvToBean<>();
-			csvToBean.setMappingStrategy(strategy);
-			csvToBean.setCsvReader(csvReader);
-			List<Order> list = csvToBean.parse();
-			
-			for(int i=0; i<list.size(); i++) {
-				Order order= list.get(i);
-				order.setId(order.getOrderId());
-				order.setFilename(fileName);
-				order.setLine(i);
-			}*/
-			
 			String[] nextRecord;
 			int lineCount = 1;
 			csvReader.readNext();
